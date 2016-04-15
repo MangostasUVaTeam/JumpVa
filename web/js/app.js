9db1f3268aa0694032cb78d65f29f008eb243f7a
Sergio García <garciparedes@gmail.com>
2016-04-15 18:51:10 +0200

    Creado panel superior para no tener que cambiar entre usuarios escribiendo en el codigo y creados componentes de la barra superior

--- web/js/app.js
+++ web/js/app.js
@@ -1,5 +1,28 @@
 (function (){
+
     var app = angular.module('jumpVa', [ ]);
 
-    
+    app.value('logedUser', {
+        name: "Paco",
+        carrier: true
+    });
+
+
+    app.controller("SearchController",['$scope','logedUser', function($scope,logedUser){
+        this.search = function(text){
+            console.log(logedUser);
+            console.log(text);
+        };
+    }]);
+
+
+    app.controller("NewDeliveryController",['$scope', 'logedUser', function($scope, logedUser){
+        this.logedUser = logedUser;
+    }]);
+
+
+
+    app.controller("ToolController", ['$scope', 'logedUser', function($scope, logedUser){
+        this.settings = logedUser;
+    }]);
 })();
