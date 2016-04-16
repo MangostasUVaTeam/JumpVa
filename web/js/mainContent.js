28eb32919e99d99eae21a34d13c4eb3b493c7828
Sergio García <garciparedes@gmail.com>
2016-04-16 10:26:28 +0200

    Creado menu desplegable para los envios en la vista principal

--- web/js/mainContent.js
+++ web/js/mainContent.js
@@ -2,4 +2,20 @@
 
     var app = angular.module('mainContent', [ ]);
 
+    app.controller("DeliveryController", [ '$scope',function($scope){
+        deliveryCtrl = this;
+
+        deliveryCtrl.delivery = {
+            dropdown: false
+        };
+
+        deliveryCtrl.toggleDropdown = function(){
+            if (deliveryCtrl.delivery.dropdown){
+                deliveryCtrl.delivery.dropdown = false;
+            } else {
+                deliveryCtrl.delivery.dropdown = true;
+            }
+        };
+    }]);
+
 })();
