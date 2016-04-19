ddcb4d2e26bae6bdcfc7229e01a917d27e575585
Sergio García <garciparedes@gmail.com>
2016-04-19 17:08:02 +0200

    Añadidos elementos a la vista e incluida libreria para google maps

--- web/js/search-deliveries.js
+++ web/js/search-deliveries.js
@@ -5,6 +5,30 @@
     app.controller('SearchDeliveriesController', function ($scope, $uibModalInstance, logedUser  ) {
         $scope.logedUser = logedUser;
 
+        $scope.pujas = [
+                {
+                    transportista: "Paco",
+                    valoracion: 3,
+                    precio: 50
+                },
+                {
+                    transportista: "Juan",
+                    valoracion: 3,
+                    precio: 65
+                },
+                {
+                    transportista: "Luis",
+                    valoracion: 3,
+                    precio: 32
+                },
+                {
+                    transportista: "Miguel",
+                    valoracion: 3,
+                    precio: 45
+                }
+        ];
+
+
         $scope.cancel = function () {
           $uibModalInstance.dismiss('cancel');
         };
