b58ea64f12fdcfdd85e78fb5f1fbf31e48d9ed4b
Sergio García <garciparedes@gmail.com>
2016-04-21 22:10:17 +0200

    Modularizado codigo y corregido fallo de tamaño de los hitos

--- web/js/app.js
+++ web/js/app.js
@@ -2,7 +2,7 @@
 
     var app = angular.module('jumpva', ['ngRoute','ui.bootstrap','ngAnimate',
     'main', 'toolbar','mainContent', 'login', 'signin','unassigned-delivery',
-    'completed-delivery','search-deliveries', 'inprogress-delivery' ]);
+    'completed-delivery','search-deliveries', 'inprogress-delivery','millestone-delivery' ]);
 
     app.config(['$routeProvider',
         function($routeProvider) {
--- web/js/inprogress-delivery.js
+++ web/js/inprogress-delivery.js
@@ -2,15 +2,5 @@
 
     var app = angular.module('inprogress-delivery', ['jumpva']);
 
-    app.controller("InprogressController", function ($scope) {
-      this.nuevoHito ={};
 
-      this.addHito=function(delivery){
-        console.log(this.nuevoHito.mensaje);
-        this.nuevoHito.time = Date.now();
-        delivery.hitos.push(this.nuevoHito);
-        this.nuevoHito = {};
-      };
-
-    });
 })();
