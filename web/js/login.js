544939009791b1e3a4bb476584968012e431b150
Sergio García <garciparedes@gmail.com>
2016-05-07 12:18:55 +0200

    Creado esqueleto de petición get

--- web/js/login.js
+++ web/js/login.js
@@ -1,18 +1,22 @@
 (function (){
 
-    var app = angular.module('login', ['jumpva']);
+    var app = angular.module('login', []);
 
 
-    app.controller("LoginController", function($scope,$location,$uibModalInstance,logedUser){
+    app.controller("LoginController", function($scope,$location,$uibModalInstance,logedUser, apiRequest){
 
         var loginCtrl = this;
 
 
-        $scope.login = function(){
+        $scope.login = function(loginRequest){
             $location.path('/main');
             $uibModalInstance.close('main');
-
+            //apiRequest.getData();
         };
+        
+        apiRequest.getData(function(dataResponse) {
+            console.log(dataResponse);
+        });
 
         $scope.cancel = function(){
             $uibModalInstance.close('cancel');
--- web/js/provider.js
+++ web/js/provider.js
@@ -2,5 +2,21 @@
 
     var app = angular.module('provider', []);
 
+    app.constant('rootPath', 'api/');
 
+    app.service('loginRequest', function($http) {
+        delete $http.defaults.headers.common['X-Requested-With'];
+        this.getData = function(callbackFunc) {
+            $http({
+                method: 'GET',
+                url: 'api/helloworld',
+                headers: {'Authorization': 'Bearer token=xxxxYYYYZzzz'}
+             }).success(function(data){
+                // With the data succesfully returned, call our callback
+                callbackFunc(data);
+            }).error(function(){
+                alert("error");
+            });
+        }
+    });
 })();
