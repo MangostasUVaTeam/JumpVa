77dd45a3642ba295378e7dfd73545deb89fd7d3f
Alberto Amigo <albamigh@gmail.com>
2016-05-10 16:33:26 +0200

    Poblacion TipoEnvio

---
+++ database/scriptPoblacionBD.sql
@@ -0,0 +1,10 @@
+insert into TipoEnvio values (0, 'Paquete');
+insert into TipoEnvio values (1, 'Animales');
+insert into TipoEnvio values (2, 'Liquidos');
+insert into TipoEnvio values (3, 'Vehiculos');
+
+
+insert into TipoHito values (0, 'Paquete');
+insert into TipoHito values (1, 'Animales');
+insert into TipoHito values (2, 'Liquidos');
+insert into TipoHito values (3, 'Vehiculos');
