72afcd3ffbc4d5177f8e1e431fd9a206f86a71ba
Sergio García <garciparedes@gmail.com>
2016-05-11 12:35:11 +0200

    Hechas peticiones a la base de datos de hitos y pujas

--- src/java/database/QueryBid.java
+++ src/java/database/QueryBid.java
@@ -5,6 +5,12 @@
  */
 package database;
 
+import database.columns.ColumnsPuja;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import server.model.shipment.bid.Bid;
 
@@ -13,10 +19,60 @@ import server.model.shipment.bid.Bid;
  * @author garciparedes
  */
 public class QueryBid {
-        protected static List<Bid> getBidList(int shipmentId){
-        return null;
+    
+    private static final String QUERY_BID = 
+            "SELECT * FROM PUJA WHERE PUJA.NROENVIO = ? AND PUJA.ACEPTADO = 't'";
+    
+    private static final String QUERY_BIDLIST = 
+            "SELECT * FROM PUJA WHERE PUJA.NROENVIO = ?";
+    
+    protected static List<Bid> getBidList(int shipmentId) throws SQLException{
+ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        
+        PreparedStatement ps = connection.prepareStatement(QUERY_BIDLIST);
+        
+        ps.setInt(1, shipmentId);
+        
+        ResultSet rs = ps.executeQuery();
+        
+        List<Bid> bidList = new ArrayList();
+        Bid bid;
+        if (rs.next()) {
+            bid = new Bid(
+                    rs.getString(ColumnsPuja.EMAIL.toString()),
+                    rs.getDouble(ColumnsPuja.OFERTA.toString())
+            );
+            bidList.add(bid);
+        }
+        
+        rs.close();
+        ps.close(); 
+        pool.freeConnection(connection);
+        return bidList; 
     }
-            protected static Bid getAssignedBid(int shipmentId){
-        return null;
+    
+    protected static Bid getAssignedBid(int shipmentId) throws SQLException{
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        
+        PreparedStatement ps = connection.prepareStatement(QUERY_BID);
+        
+        ps.setInt(1, shipmentId);
+        
+        ResultSet rs = ps.executeQuery();
+        
+        Bid bid = null;
+        if (rs.next()) {
+            bid = new Bid(
+                    rs.getString(ColumnsPuja.EMAIL.toString()),
+                    rs.getDouble(ColumnsPuja.OFERTA.toString())
+            );
+        }
+        
+        rs.close();
+        ps.close(); 
+        pool.freeConnection(connection);
+        return bid;    
     }
 }
--- src/java/database/QueryMilestone.java
+++ src/java/database/QueryMilestone.java
@@ -5,8 +5,15 @@
  */
 package database;
 
+import database.columns.ColumnsHito;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import server.model.shipment.milestone.Milestone;
+import server.model.shipment.milestone.MilestoneType;
 
 /**
  *
@@ -14,7 +21,34 @@ import server.model.shipment.milestone.Milestone;
  */
 public class QueryMilestone {
     
-    protected static List<Milestone> getMilestoneList(int shipmentId){
-        return null;
+    private static final String QUERY_MILESTONELIST = 
+            "SELECT * FROM HITO WHERE HITO.NROENVIO = ? ORDER BY NROHITO";
+
+    
+    protected static List<Milestone> getMilestoneList(int shipmentId) throws SQLException{
+       ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        
+        PreparedStatement ps = connection.prepareStatement(QUERY_MILESTONELIST);
+        
+        ps.setInt(1, shipmentId);
+        
+        ResultSet rs = ps.executeQuery();
+        
+        List<Milestone> milestoneList = new ArrayList();
+        Milestone bid;
+        while (rs.next()) {
+            bid = new Milestone(
+                    rs.getDate(ColumnsHito.FCREACION.toString()),
+                    MilestoneType.COMMENT,
+                    rs.getString(ColumnsHito.MENSAJE.toString())
+            );
+            milestoneList.add(bid);
+        }
+        
+        rs.close();
+        ps.close(); 
+        pool.freeConnection(connection);
+        return milestoneList; 
     }
 }
