805b6cea428bfc80248c7b44654a5a2d075c7533
Sergio García <garciparedes@gmail.com>
2016-05-30 11:09:03 +0200

    Añadida validación de formulario para añadir un hito limitando la entrada a un mínimo de 6 caracteres

--- web/js/millestone-delivery.js
+++ web/js/millestone-delivery.js
@@ -4,7 +4,7 @@
 
     app.controller("MillestoneController", function (user, $scope) {
         var self = this;
-        
+
         self.getMilestoneType = function(milestone){
             switch(milestone.authorEmail){
                 case '':
@@ -15,25 +15,27 @@
                     return 'warning';
             };
         }
-        
+
         self.addMilestone = function(shipment){
-              console.log(self.newMilestone.body);
+            console.log(self.newMilestone.body);
 
-              self.newMilestone.date = Date.now();
+            self.newMilestone.date = Date.now();
 
-              user.postNewHitoToShipment(shipment.id, self.newMilestone)
-                      .then(handleRequest(),handleBadRequest());
-
+            if($scope.reviewForm.$valid){
+                user.postNewHitoToShipment(shipment.id, self.newMilestone)
+                        .then(handleRequest(),handleBadRequest());
+            }
         };
-        
+
         function handleRequest(res) {
             $scope.delivery.milestoneList.push(self.newMilestone);
             self.newMilestone = {};
+            $scope.reviewForm.$setPristine();
+            $scope.reviewForm.$setUntouched();
 
         };
-        
+
         function handleBadRequest(res) {
-            self.newMilestone = {};
 
         };
 
